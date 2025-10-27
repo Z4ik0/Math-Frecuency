152f4e386d322551411655eed0bbc5b95706cc21
Irving Cruz Chávez <ic3386941@gmail.com>
2025-10-27 10:43:35 -0600

    [ADD]: Se instalaron dependencias necesarias, crearon archivos iniciales y navegacion entre pantallas.

---
+++ Navegation/Navegator.tsx
@@ -0,0 +1,19 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { HomeScreen } from "../Screens/HomeScreen";
+import { GraficasScreen } from "../Screens/GraficasScreen";
+import { CreditScreen } from "../Screens/CreditScreen";
+
+const Tab = createBottomTabNavigator();
+
+export function Navegation() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Graficas" component={GraficasScreen} />
+        <Tab.Screen name="Creditos" component={CreditScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ Screens/CreditScreen.tsx
@@ -0,0 +1,3 @@
+export const CreditScreen = () => {
+  return <></>;
+};
